6b1f69cc3be28266bc042edae7216ef2c0c12bc0
Frey <Frey0xD@gmail.com>
2023-01-10 01:37:20 -0500

    Boolen in Py

---
+++ PythonHacking/boolean.py
@@ -0,0 +1,37 @@
+print(bool(0)) # False
+print(bool(1)) # True
+valid = True
+not_valid = False
+
+print(valid)
+print(not_valid)
+
+print(valid == True)
+print(not_valid == True)
+
+print(valid != True)
+print(not_valid != True)
+
+print(not valid)
+print(not not_valid)
+
+print((10 > 9) == True)
+print((10 == 10) == True)
+print((10 != 10 ) == True)
+print((10 <= 9) == True)
+print((10 < 9) == True)
+print((10 >= 9) == True)
+
+print(10 > 9)
+print(10 == 10)
+print(10 != 10)
+print(10 <= 9)
+print(10 < 9)
+print(10 >= 9)
+
+print('-----')
+print(10 > 5 and 10 < 5)
+print(10 > 5 or 10 < 5)
+print(bool(0) == True)
+print(bool(1) == True)
+
