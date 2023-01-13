91ed722d5707d37e9cb8bf0f366a1a3d77be5c48
Frey <Frey0xD@gmail.com>
2023-01-13 06:59:04 -0500

    COndition

---
+++ PythonHacking/conditions.py
@@ -0,0 +1,64 @@
+if True:
+    print("True") 
+
+if False:
+    print("False")
+
+if not False:
+    print("Will Print")
+
+if 1 < 1:
+    print("1 < 1")
+elif 1 <= 0:
+    print("1<=1")
+else:
+    print("None")
+if 1 > 0 and 0 < 1:
+    print("1 > 0 and 0 < 1")
+if (1 < 0 or 0 < 1) or 1 == 0:
+    print("1 < 0 or 0 < 1")
+if 0 < 1: print("0 < 1")
+print("1 >= 1") if 1 >= 1 else print("1 < 1")
+if 1 >= 1:
+    print("1 >= 1")
+else:
+    print("1 < 1")
+
+if 0 <= 0:
+    print("1")
+elif 0 < 1:
+    print("2")
+else:
+    print("3")
+
+print("1") if 0==0 else print("2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
