c3b401227d132a4ff06f9c2c7ff70785c45648b9
Frey <Frey0xD@gmail.com>
2023-01-16 09:55:58 -0500

    Input

---
+++ PythonHacking/input.py
@@ -0,0 +1,14 @@
+test = input()
+print(test)
+
+test = input("Enter IP:-")
+print(test)
+
+while True:
+    test = input("Enter IP:-")
+    print(">>> {}".format(test))
+    if test == "exit":
+        break
+    else:
+        print("Exploiting....")
+
