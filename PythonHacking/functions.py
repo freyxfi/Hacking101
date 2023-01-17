d2a7b6ebd3577c3ff1de09ddb8c67d8b85b39c3d
Frey <Frey0xD@gmail.com>
2023-01-17 10:12:37 -0500

    Functions

--- PythonHacking/functions.py
+++ PythonHacking/functions.py
@@ -26,3 +26,58 @@ def function5(s1 = "Default"):
     print("{}".format(s1))
 
 function5()
+function5("Anything")
+
+def function6(s1, *more):
+    print("{} {}".format(s1, " ".join([s for s in more])))
+
+function6("yo yo")
+function6("lol", "this", "will", "work")
+
+def function7(**ks):
+    for a in ks:
+        print(a, ks[a])
+        
+function7(a="1", b="2", c="3")
+
+def function8(s,f,i,l):
+    print(type(s))
+    print(type(f))
+    print(type(i))
+    print(type(l))
+
+function8("Hello", 3.0, 3, ["L", "i", "s", "t"])
+
+v = 100
+print(v)
+def function9():
+    global v
+    v += 1
+    #v  = 50
+    print(v)
+
+function9()
+print(v)
+
+def function10():
+    print("Hello from the function10")
+
+def function11():
+    function10()
+    print("Hello from the function11")
+
+function11()
+
+def function12(x):
+    print(x)
+    if x > 0:
+        function12(x-1)
+
+function12(5)
+
+def function13(x):
+    while x >= 0:
+        print(x)
+        x -= 1
+
+function13(5)
