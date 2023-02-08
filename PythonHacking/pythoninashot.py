f79ecc4e4691d9e3e8eb4d2ddc96c8121516a3c9
Frey <Frey0xD@gmail.com>
2023-02-07 19:08:31 -0500

    python in a shot

---
+++ PythonHacking/pythoninashot.py
@@ -0,0 +1,16 @@
+# i will skip the introduction because i think it is not useful 
+
+# this is how you print
+print("Have fun")
+print("Python is a gernal purpose language")
+#Let's have some maths operations 
+print(3+2+3)
+print(12*2)
+print(12/4)
+#let me explain you about int and float 
+"""
+so here --> 34,2,4,1 = is int
+4.23,6.2,6.24 = is float 
+"""
+
+
